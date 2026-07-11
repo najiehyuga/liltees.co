d2e7ac159b6a1f5c50c186b3ed4071bef743094e
Najiy <najienajienajie99@gmail.com>
2026-07-11 22:57:21 +0700

    fix

--- js/supabase.js
+++ js/supabase.js
@@ -4,10 +4,8 @@ const SUPABASE_URL =
 const SUPABASE_KEY =
 "sb_publishable_xxxxxxxxxxxxxxxxx";
 
-// Ambil createClient dari library
 const { createClient } = supabase;
 
-// Buat koneksi
 const db = createClient(
     SUPABASE_URL,
     SUPABASE_KEY
