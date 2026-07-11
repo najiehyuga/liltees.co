ba49f30f564fd3b9da1aaa95287135d0224b32d3
Najiy <najienajienajie99@gmail.com>
2026-07-11 21:54:09 +0700

    Add admin panel

---
+++ js/supabase.js
@@ -0,0 +1,11 @@
+const SUPABASE_URL =
+"https://xhwpyzytwaiymeslhlx.supabase.co";
+
+const SUPABASE_KEY =
+"sb_publishable_xxxxxxxxxxxxxxxxx";
+
+const supabase =
+window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
