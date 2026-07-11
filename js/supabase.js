074e0d7d34f7c71433a76db6f336e34be3be461c
Najiy <najienajienajie99@gmail.com>
2026-07-12 01:36:50 +0700

    fix

---
+++ js/supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://xhwpyzytwaiaymeslhlx.supabase.co";
+
+const SUPABASE_ANON_KEY = "sb_publishable_Bgc1eoZadhu4U7IIFgtfFw_mXtxhD9e";
+
+const db = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
