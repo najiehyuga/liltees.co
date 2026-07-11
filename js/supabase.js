f76318470f4da6de575c9701bc57cae50250b61f
Najiy <najienajienajie99@gmail.com>
2026-07-11 23:00:25 +0700

    Update Supabase publishable key

--- js/supabase.js
+++ js/supabase.js
@@ -2,7 +2,7 @@ const SUPABASE_URL =
 "https://xhwpyzytwaiaymeslhlx.supabase.co";
 
 const SUPABASE_KEY =
-"sb_publishable_xxxxxxxxxxxxxxxxx";
+"sb_publishable_Bgc1eoZadhu4U7IIFgtfFw_mXtxhD9e";
 
 const { createClient } = supabase;
 
